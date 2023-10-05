2cfb4e27dff529617723cff906d73d64813c3554
saugat poudel <saugat.poudel478@gmail.com>
2023-10-05 21:35:59 +0545

    barchart bug fixed

--- bar.py
+++ bar.py
@@ -10,7 +10,6 @@ bargraph_layout = dcc.Graph(
     style={'width': '50%', 'height': '300px'},
 )
 
-# Define the callback for the bar graphz
 def bargraph_callback(app, data, default_processors):
     @app.callback(
         Output("graph", "figure"),
@@ -23,32 +22,20 @@ def bargraph_callback(app, data, default_processors):
             selected_processors = [selected_processors]  # Convert to a list if it's a single value
 
         # Calculate both max and min prices for each CPU processor
-        price_stats_df = data.groupby('cpu_processor').agg({'price_eur': ['max', 'min']}).reset_index()
-        price_stats_df.columns = ['cpu_processor', 'max_price_eur', 'min_price_eur']
+        price_stats_df = data.groupby('cpu_processor').agg({'price_eur': ['max', 'min'], 'name': ['first', 'last']}).reset_index()
+        price_stats_df.columns = ['cpu_processor', 'max_price_eur', 'min_price_eur', 'laptop_name_max', 'laptop_name_min']
 
         filtered_df = price_stats_df[price_stats_df['cpu_processor'].isin(selected_processors)]
 
-        laptop_names_max = []
-        laptop_names_min = []
-        for processor in selected_processors:
-            max_idx = data[data['cpu_processor'] == processor]['price_eur'].idxmax()
-            min_idx = data[data['cpu_processor'] == processor]['price_eur'].idxmin()
-            laptop_names_max.append(data.loc[max_idx, 'name'])
-            laptop_names_min.append(data.loc[min_idx, 'name'])
-
-        filtered_df['laptop_name_max'] = laptop_names_max
-        filtered_df['laptop_name_min'] = laptop_names_min
-        
         hover_template_max = '<b>CPU Processor</b>: %{x}' + '<br>' + \
-                        '<b>Max Price</b>: %{y:.2f} EUR' + '<br>' + \
-                        '<b>Laptop Name (Max)</b>: %{customdata[0]}' + '<extra></extra>'
+                            '<b>Max Price</b>: %{y:.2f} EUR' + '<br>' + \
+                            '<b>Laptop Name (Max)</b>: %{customdata[0]}' + '<extra></extra>'
         hover_template_min = '<b>CPU Processor</b>: %{x}' + '<br>' + \
-                        '<b>Min Price</b>: %{y:.2f} EUR' + '<br>' + \
-                        '<b>Laptop Name (Min)</b>: %{customdata[1]}' + '<extra></extra>'
-                        
-        
-        customdf = np.stack((filtered_df['laptop_name_max'],filtered_df['laptop_name_min']))     
-            
+                            '<b>Min Price</b>: %{y:.2f} EUR' + '<br>' + \
+                            '<b>Laptop Name (Min)</b>: %{customdata[1]}' + '<extra></extra>'
+
+        customdf = np.stack((filtered_df['laptop_name_max'], filtered_df['laptop_name_min']))
+
         fig = px.bar(
             filtered_df,
             x="cpu_processor",
@@ -58,13 +45,12 @@ def bargraph_callback(app, data, default_processors):
             labels={"max_price_eur": "Max Price", "min_price_eur": "Min Price"},
             custom_data=customdf
         )
-
         fig.update_yaxes(title_text="Price")
         fig.update_xaxes(title_text="CPU_Processor")
         y_range = [0, max(price_stats_df['max_price_eur'].max(), price_stats_df['min_price_eur'].max())]
+
         fig.update_layout(yaxis=dict(range=y_range))
         fig.update_traces(hovertemplate=hover_template_max, selector=dict(name='max_price_eur'))
         fig.update_traces(hovertemplate=hover_template_min, selector=dict(name='min_price_eur'))
-
 
         return fig
